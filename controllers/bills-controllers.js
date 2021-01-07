5d4caa2e34f6a5fd97c4a48632aab6199f7c8a12
DanielO199 <futbolcup123@onet.pl>
2021-01-07 15:12:47 +0100

    add controller for sending user savingsData

--- controllers/bills-controllers.js
+++ controllers/bills-controllers.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const Bill = require('../models/bill');
+const Transaction = require('../models/transaction');
 const CONSTS = require('../utils/constants');
 const _generateBillNumber = require('../utils/generate-billNumber');
 
@@ -33,6 +34,42 @@ const getAllUserFunds = async (req, res) => {
 
 	return res.status(200).json({
 		funds
+	});
+};
+
+const getUserSavings = async (req, res) => {
+	const userId = req.params.uid;
+
+	let outgoingTransfers;
+	try {
+		outgoingTransfers = await Transaction.find({ sender: userId });
+	} catch (err) {
+		return res.status(500).json({ message: 'Server Failed' });
+	}
+
+	let incomingTransfers;
+	try {
+		incomingTransfers = await Transaction.find({ receiver: userId });
+	} catch (err) {
+		return res.status(500).json({ message: 'Server Failed' });
+	}
+
+	let moneyFromOutgoingTransfers = outgoingTransfers.reduce(
+		(acc, item) => acc + item.money,
+		0
+	);
+	let moneyFromIncomingTransfers = incomingTransfers.reduce(
+		(acc, item) => acc + item.money,
+		0
+	);
+
+	const savingsData = [
+		{ name: 'outgoing', money: moneyFromOutgoingTransfers },
+		{ name: 'incoming', money: moneyFromIncomingTransfers }
+	];
+
+	return res.status(200).json({
+		savingsData
 	});
 };
 
@@ -87,4 +124,5 @@ const _createBillNumber = async () => {
 
 exports.getAllBillsByUserId = getAllBillsByUserId;
 exports.getAllUserFunds = getAllUserFunds;
+exports.getUserSavings = getUserSavings;
 exports.createNewBill = createNewBill;
--- routes/bill-routes.js
+++ routes/bill-routes.js
@@ -5,8 +5,10 @@ const billsControllers = require('../controllers/bills-controllers');
 const router = express.Router();
 // Get all bills by user id
 router.get('/:uid', billsControllers.getAllBillsByUserId);
-// Get all bills by user id
+// Get all user funds
 router.get('/funds/:uid', billsControllers.getAllUserFunds);
+// Get user savings
+router.get('/savings/:uid', billsControllers.getUserSavings);
 //Create new bill and assing it to client
 router.post('/', billsControllers.createNewBill);
 
